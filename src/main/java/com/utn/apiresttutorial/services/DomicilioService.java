9551207b8e82767a0b25828aca7b5d5d56a6f0bc
LeitoGonzalez <141784522+LeitoGonzalez@users.noreply.github.com>
2023-10-05 13:45:42 -0300

    arreglado jej

--- src/main/java/com/utn/apiresttutorial/services/DomicilioService.java
+++ src/main/java/com/utn/apiresttutorial/services/DomicilioService.java
@@ -1,7 +1,6 @@
 package com.utn.apiresttutorial.services;
 
 import com.utn.apiresttutorial.entities.Domicilio;
-import com.utn.apiresttutorial.repositories.BaseRepository;
 
 public interface DomicilioService extends BaseService<Domicilio, Long> {
 }
