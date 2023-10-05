539b3e64642fa1efd7093fa9dbfa7e3429a7bfdb
LeitoGonzalez <141784522+LeitoGonzalez@users.noreply.github.com>
2023-10-05 14:21:13 -0300

    aa jej

--- src/main/java/com/utn/apiresttutorial/controllers/DomicilioController.java
+++ src/main/java/com/utn/apiresttutorial/controllers/DomicilioController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin("*")
 @RequestMapping(path = "api/v1/domicilios")
 public class DomicilioController extends BaseControllerImpl<Domicilio, DomicilioServiceImpl> {
 }
